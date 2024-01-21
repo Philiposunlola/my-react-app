490b28317b9f8d82592682e90bd30b8f00a8800d
Philip <philiposun@gmail.com>
2024-01-22 00:27:11 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <div>
+        <p>Nicholas Osunlola is a Tech bro and a good boy</p>
+        <p>God is the Greatest, he has done wonders for me and my brothers and he will continue to do more as time goes on</p>
+      </div>
+      
     </div>
   );
 }
