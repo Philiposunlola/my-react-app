0c32b41bc55f260e7e8f4b768ecb6dd89be056a9
Philip <philiposun@gmail.com>
2024-01-27 01:17:44 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const myFirstElement = <h1></h1>
+const myFirstElement = <h1>philip</h1>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(myFirstElement);
